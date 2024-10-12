c45205fe531f5f82434771d1cca24d08fa73428b
Sam Roberts <sam@Sams-MacBook-Air.local>
2024-10-12 07:14:20 +0100

    copy / paste code from previous project and edit for UK date format input

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def main():
                 print("Invalid postcode. Please try again.")
     except EOFError:  # Handle Ctrl+D (End of Input)
         print("\nInput interrupted. Exiting...")
+    date_of_match = convert_date_format()
+    print(date_of_match)
+    
+    
 
 
 def postcode_validation(postcode):
@@ -32,6 +36,24 @@ def postcode_validation(postcode):
         print(f"Error occurred: {e}")
         return False  # Return False if there's an error during the request
 
+def convert_date_format():
+
+    print("Enter the date you wish to see a match")
+    while True:
+        date = input("Date format must be DD/MM/YYYY: ")
+        try:
+            if "/" in date:
+                day, month, year = date.split("/")
+                day = int(day)
+                month = int(month)
+                year = int(year)
+                if day > 31 or month > 12 or year < 2024:
+                    raise ValueError
+                return f"{year}-{month:02d}-{day:02d}"
+            else:
+                print("date format incorrect")
+        except ValueError:
+            continue
 
 if __name__ == "__main__":
     main()
