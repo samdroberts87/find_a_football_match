94a6f911411ff5860627371e63caa50f85889e0e
Sam Roberts <sam@Sams-MacBook-Air.local>
2024-10-12 06:36:28 +0100

    postcode validation function added and tests passing

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import requests
+import time
 
 def main():
-    postcode = input("Enter Your postcode")
-    postcode_validation(postcode)
+    print("Welcome to Find A Football Match")
+    time.sleep(1)
+    print("Let's find you a football match to watch!")
+    try:
+        while True:
+            postcode = input("Enter your postcode: ")
+            if postcode_validation(postcode):
+                print("Valid postcode.")
+                break  # Exit the loop once a valid postcode is entered
+            else:
+                print("Invalid postcode. Please try again.")
+    except EOFError:  # Handle Ctrl+D (End of Input)
+        print("\nInput interrupted. Exiting...")
+        
 def postcode_validation(postcode):
-    pass
+    try:
+        postcode_api = "https://api.postcodes.io/postcodes/"
+        response = requests.get(f"{postcode_api}{postcode}")
+        
+        if response.status_code == 200:
+            data = response.json()
+            return data["status"] == 200  # Return True if valid postcode
+        else:
+            return False  # Invalid postcode
+    except requests.RequestException as e:
+        print(f"Error occurred: {e}")
+        return False  # Return False if there's an error during the request
 
 if __name__ == "__main__":
     main()
