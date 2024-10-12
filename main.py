e098f15962c04183ad966613f08495d7557ef05b
Sam Roberts <sam@Sams-MacBook-Air.local>
2024-10-12 06:08:21 +0100

    add main, postcode_validation functions to main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+
+def main():
+    postcode = input("Enter Your postcode")
+    postcode_validation(postcode)
+def postcode_validation(postcode):
+    pass
+
+if __name__ == "__main__":
+    main()
