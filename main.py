405bf51dfe7fb76043f1fd87163a5327ba3325aa
Sam Roberts <sam@Sams-MacBook-Air.local>
2024-10-13 19:07:18 +0100

    add ticket purchase link and make output look nicer

--- main.py
+++ main.py
@@ -4,28 +4,29 @@ from geopy.geocoders import Nominatim
 from geopy.distance import great_circle
 from config import SPORT_HIGHLIGHTS_API_KEY
 import sys
+import termcolor
 
 premier_league_teams = {
-    "Liverpool": "L40TH",  # Anfield
-    "Manchester City": "M113FF",  # Etihad Stadium
-    "Arsenal": "N51BU",  # Emirates Stadium
-    "Chelsea": "SW61HS",  # Stamford Bridge
-    "Aston Villa": "B66HE",  # Villa Park
-    "Brighton": "BN19BL",  # Amex Stadium
-    "Newcastle": "NE14ST",  # St James' Park
-    "Fulham": "SW66HH",  # Craven Cottage
-    "Tottenham": "N170AP",  # Tottenham Hotspur Stadium
-    "Nottingham Forest": "NG25FJ",  # City Ground
-    "Brentford": "TW80NT",  # Gtech Community Stadium
-    "West Ham": "E202ST",  # London Stadium
-    "Bournemouth": "BH77AF",  # Vitality Stadium
-    "Manchester United": "M160RA",  # Old Trafford
-    "Leicester": "LE27FL",  # King Power Stadium
-    "Everton": "L44EL",  # Goodison Park
-    "Ipswich": "IP12DA",  # Portman Road
-    "Crystal Palace": "SE256PU",  # Selhurst Park
-    "Southampton": "SO145FP",  # St Mary's Stadium
-    "Wolves": "WV14QR",  # Molineux Stadium
+    "Liverpool": ["L40TH", "https://www.liverpoolfc.com/tickets/tickets-availability"],  # Anfield
+    "Manchester City": ["M113FF", "https://www.mancity.com/tickets/mens"],  # Etihad Stadium
+    "Arsenal": ["N51BU", "https://www.arsenal.com/tickets"],  # Emirates Stadium
+    "Chelsea": ["SW61HS", "https://www.chelseafc.com/en/ticket-information"],  # Stamford Bridge
+    "Aston Villa": ["B66HE", "https://www.avfc.co.uk/fans/fans-charter/ticketing/"],  # Villa Park
+    "Brighton": ["BN19BL", "https://tickets.brightonandhovealbion.com/"],  # Amex Stadium
+    "Newcastle": ["NE14ST", "https://tickets.manutd.com/"],  # St James' Park
+    "Fulham": ["SW66HH", "https://www.fulhamfc.com/tickets-and-hospitality"],  # Craven Cottage
+    "Tottenham": ["N170AP", "https://www.tottenhamhotspur.com/tickets"],  # Tottenham Hotspur Stadium
+    "Nottingham Forest": ["NG25FJ", "https://www.eticketing.co.uk/nottinghamforest"],  # City Ground
+    "Brentford": ["TW80NT", "https://www.eticketing.co.uk/brentfordfc"],  # Gtech Community Stadium
+    "West Ham": ["E202ST", "https://www.eticketing.co.uk/whufc"],  # London Stadium
+    "Bournemouth": ["BH77AF", "https://www.afcb.co.uk/tickets/"],  # Vitality Stadium
+    "Manchester United": ["M160RA", "https://www.manutd.com/tickets"],  # Old Trafford
+    "Leicester": ["LE27FL", "https://www.premierleague.com/www.lcfc.com/season-tickets"],  # King Power Stadium
+    "Everton": ["L44EL", "https://www.evertonfc.com/tickets/latest"],  # Goodison Park
+    "Ipswich": ["IP12DA", "https://tickets.itfc.co.uk/"],  # Portman Road
+    "Crystal Palace": ["SE256PU", "https://www.eticketing.co.uk/cpfc/"],  # Selhurst Park
+    "Southampton": ["SO145FP", "https://www.premierleague.com/southamptonfc.com/match-tickets"],  # St Mary's Stadium
+    "Wolves": ["WV14QR", "https://www.eticketing.co.uk/wolves"]  # Molineux Stadium
 }
 
 # Initialize the geocoder
@@ -33,14 +34,14 @@ geolocator = Nominatim(user_agent="postcode_distance_checker")
 
 
 def main():
-    print("Welcome to Find A Football Match")
+    termcolor.cprint("\nWelcome to Find A Football Match", "green")
     time.sleep(1)
     print("Let's find you a premier league football match to watch!")
     try:
         while True:
             postcode = input("Enter your postcode: ").replace(" ", "")
             if postcode_validation(postcode):
-                print(f"Your postcode has been validated and set at {postcode}.")
+                termcolor.cprint(f"Your postcode has been validated and set as {postcode}", "green")
                 break  # Exit the loop once a valid postcode is entered
             else:
                 print("Invalid postcode. Please try again.")
@@ -49,24 +50,25 @@ def main():
 
     date_of_match = convert_date_format()
     time.sleep(0.5)
-    print(f"Your chosen date is: {date_of_match}")
+    termcolor.cprint(f"Your chosen date is: {date_of_match}", "green")
     distance = get_travel_miles()
     time.sleep(0.5)
-    print(f"You are willing to travel: {distance:.2f} miles")
-    print(
-        "Thank you for that information. We'll get right on it. Please wait a few moments...."
+    termcolor.cprint(f"You are willing to travel: {distance:.2f} miles", "green")
+    termcolor.cprint(
+        "Please wait a few moments....", attrs=["bold"] 
     )
     # Find nearby teams
     nearby_teams = find_nearby_postcodes(postcode, distance, premier_league_teams)
     if nearby_teams:
-        print("Teams within your travel distance:")
+        termcolor.cprint("Teams within your travel distance:", "green")
         for team, team_postcode, team_distance in nearby_teams:
             print(f" - {team.title()} ({team_postcode}): {team_distance:.2f} miles")
     else:
-        print("No teams found within your travel distance.")
+        termcolor.cprint("No teams found within your travel distance.", "red")
         sys.exit(0)
 
-    print(f"Let's see which of those teams are playing at home on {date_of_match}")
+    time.sleep(1)
+    print(f"Let's see which of those teams are playing at home on {date_of_match}\n")
 
     home_teams = get_fixtures(date_of_match)
 
@@ -76,18 +78,18 @@ def main():
             available_teams.append(team)
 
     if not available_teams:
-        print(
-            f"Sorry, no matches available on {date_of_match} within {distance} miles of {postcode}"
+        termcolor.cprint(
+            f"Sorry, no matches available on {date_of_match} within {distance} miles of {postcode}", "red"
         )
     else:
         for i in available_teams:
             time.sleep(0.5)
-            url = (
-                f"https://www.google.com/maps/dir/{postcode}/{premier_league_teams[i]}"
-            )
-            print(
-                f"{i} play at home. Here's a google maps link with details of how to get there: {url}"
-            )
+            url = f"https://www.google.com/maps/dir/{postcode}/{premier_league_teams[i][0]}"
+            termcolor.cprint(f"{i} are at home", attrs=["bold"])
+            print(f"Here's the club's website to buy tickets: {premier_league_teams[i][1]}")
+            print(f"Here's a google maps link with details of how to get there: {url}")
+
+
 
 
 def postcode_validation(postcode):
@@ -154,7 +156,8 @@ def find_nearby_postcodes(target_postcode, distance_limit_miles, teams):
 
         nearby_teams = []
 
-        for team, postcode in teams.items():
+        for team, team_data in teams.items():
+            postcode = team_data[0]  # Extract the postcode from the list
             coords = get_coordinates(postcode)
             if coords:
                 distance = great_circle(target_coords, coords).miles
@@ -164,6 +167,7 @@ def find_nearby_postcodes(target_postcode, distance_limit_miles, teams):
         return nearby_teams
     except EOFError:  # Handle Ctrl+D (End of Input)
         print("\nInput interrupted. Exiting...")
+
 
 
 def get_coordinates(postcode):
