b2ad10253a117e45e171c570cac4ad5b860ee793
Sam Roberts <sam@Sams-MacBook-Air.local>
2024-10-25 21:58:22 +0100

    format to PEP8 with black

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from modules.api_requests import get_fixtures
 def main():
     try:
         play_again = True
-        while play_again:    
+        while play_again:
             termcolor.cprint("\nWelcome to Find A Football Match", "green")
             time.sleep(1)
             print("Let's find you a premier league football match to watch!")
@@ -27,21 +27,29 @@ def main():
             termcolor.cprint(f"Your chosen date is: {date_of_match}", "green")
             distance = get_travel_miles()
             time.sleep(0.5)
-            termcolor.cprint(f"You are willing to travel: {distance:.2f} miles", "green")
+            termcolor.cprint(
+                f"You are willing to travel: {distance:.2f} miles", "green"
+            )
             termcolor.cprint("Please wait a few moments....", attrs=["bold"])
 
-            nearby_teams = find_nearby_postcodes(postcode, distance, premier_league_teams)
+            nearby_teams = find_nearby_postcodes(
+                postcode, distance, premier_league_teams
+            )
 
             if nearby_teams:
                 termcolor.cprint("Teams within your travel distance:", "green")
                 for team, team_postcode, team_distance in nearby_teams:
-                    print(f" - {team.title()} ({team_postcode}): {team_distance:.2f} miles")
+                    print(
+                        f" - {team.title()} ({team_postcode}): {team_distance:.2f} miles"
+                    )
             else:
                 termcolor.cprint("No teams found within your travel distance.", "red")
                 sys.exit(0)
 
             time.sleep(1)
-            print(f"Let's see which of those teams are playing at home on {date_of_match}\n")
+            print(
+                f"Let's see which of those teams are playing at home on {date_of_match}\n"
+            )
 
             home_teams = get_fixtures(date_of_match)
             available_teams = []
@@ -62,11 +70,11 @@ def main():
                     termcolor.cprint(f"{i} are at home", "green", attrs=["bold"])
                     print(f"Buy tickets: {premier_league_teams[i][1]}")
                     print(f"Get there: {url}")
-                
+
             answer = input(f"\nWant to go again? (Y/N)").lower()
             if answer in ["y", "yes"]:
                 play_again = True
-                time.sleep(.5)
+                time.sleep(0.5)
                 termcolor.cprint("okay, let's go again", attrs=["bold"])
             else:
                 termcolor.cprint("Okay, Exiting", "red", attrs=["bold"])
@@ -75,7 +83,6 @@ def main():
     except EOFError:
         termcolor.cprint("EXITING DUE TO USER CTL+D", "red")
         sys.exit(0)
-        
 
 
 if __name__ == "__main__":
